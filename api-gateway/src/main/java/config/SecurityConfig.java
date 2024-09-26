bac5f267e9d0028c287e211dde7fad058dc66b1a
Max David <maxdavidsouza@gmail.com>
2024-09-26 16:56:31 -0300

    Ajustando o SecurityConfig para o formato lambda.

--- api-gateway/src/main/java/config/SecurityConfig.java
+++ api-gateway/src/main/java/config/SecurityConfig.java
@@ -5,24 +5,24 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
-    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity){
-        serverHttpSecurity.csrf()
-            .disable()
-            .authorizeExchange(exchange -> exchange.pathMatchers("/eureka/**")
-                .permitAll()
-                .anyExchange()
-                .authenticated()
-            )
-            .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
-        return serverHttpSecurity.build();
-
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http.csrf(csrf -> csrf.disable()
+                    .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/eureka/**")
+                        .permitAll()
+                        .anyExchange()
+                        .authenticated()
+                    )
+                    .oauth2ResourceServer(withDefaults())
+                );
+        return http.build();
     }
     
 }
